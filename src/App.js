0f92932a2005afc6b47bbefe44fb4f4375c86c2b
Eelis Hiltunen <elkku.hiltunen@gmail.com>
2023-09-28 14:56:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,91 +1,67 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-class BACCalculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gender: 'male',
-      bottles: 0,
-      weight: 0,
-      time: 0,
-      result: null,
-    };
-  }
+const BACCalculator = () => {
+  const [bottles, setBottles] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState("male");
+  const [bac, setBAC] = useState(null);
 
-  calculateBAC = () => {
-    const { gender, bottles, weight, time } = this.state;
-
-    // Constants for calculations
-    const liters = bottles * 0.33;
-    const grams = liters * 8 * 4.5;
+  const calculateBAC = () => {
+    const litres = bottles * 0.33;
+    const grams = litres * 8 * 4.5;
     const burning = weight / 10;
     const gramsLeft = grams - burning * time;
+    const bacFormula = gender === "male" ? gramsLeft / (weight * 0.7) : gramsLeft / (weight * 0.6);
 
-    let result;
-    if (gender === 'male') {
-      result = gramsLeft / (weight * 0.7);
-    } else if (gender === 'female') {
-      result = gramsLeft / (weight * 0.6);
-    }
-
-    this.setState({ result });
+    setBAC(bacFormula.toFixed(2));
   };
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    const { gender, bottles, weight, time, result } = this.state;
-
-    return (
+  return (
+    <div>
+      <h1>Blood Alcohol Content (BAC) Calculator</h1>
       <div>
-        <h1>BAC Calculator</h1>
-        <label>
-          Select Gender:
-          <select name="gender" value={gender} onChange={this.handleChange}>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Number of Bottles:
-          <input
-            type="number"
-            name="bottles"
-            value={bottles}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Body Weight (kg):
-          <input
-            type="number"
-            name="weight"
-            value={weight}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Time Since First Drink (hours):
-          <input
-            type="number"
-            name="time"
-            value={time}
-            onChange={this.handleChange}
-          />
-        </label>
-        <br />
-        <button onClick={this.calculateBAC}>Calculate BAC</button>
-        <p>
-          {result !== null && `Estimated BAC: ${result.toFixed(4)}`}
-        </p>
+        <label htmlFor="bottles">Number of Bottles:</label>
+        <input
+          type="number"
+          id="bottles"
+          value={bottles}
+          onChange={(e) => setBottles(e.target.value)}
+        />
       </div>
-    );
-  }
-}
+      <div>
+        <label htmlFor="weight">Weight (kg):</label>
+        <input
+          type="number"
+          id="weight"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="time">Time Since Drinking (hours):</label>
+        <input
+          type="number"
+          id="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="gender">Gender:</label>
+        <select
+          id="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <button onClick={calculateBAC}>Calculate BAC</button>
+      {bac !== null && <p>Your BAC is: {bac}</p>}
+    </div>
+  );
+};
 
 export default BACCalculator;
